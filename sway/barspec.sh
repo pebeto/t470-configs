85057d09cc7613c110cf065e6cf249bccb101579
Jose Esparza <pebeto99@gmail.com>
2025-01-25 10:20:35 -0500

    Adding volume and brightness info/controls

--- sway/barspec.sh
+++ sway/barspec.sh
@@ -8,18 +8,28 @@ get_battery_info() {
         not)
             battery_info="NA"
             ;;
-        discharging)
+        discharging|high|critical)
 	        battery_info=$(acpiconf -i $1 | awk '/Remaining capacity/{print $3}')
             ;;
     esac
-
 	echo $battery_info
 }
-
-datentime=$(date "+%Y-%m-%d %H:%M:%S")
-
 internal_battery_percentage="IntBAT: $(get_battery_info 0)"
 external_battery_percentage="ExtBAT: $(get_battery_info 1)"
 battery_info="$internal_battery_percentage - $external_battery_percentage"
 
-echo "$battery_info | $datentime"
+get_volume_info() {
+    actual_volume=$(mixer vol.volume | awk -F: '{print $2}')
+    echo $actual_volume
+}
+volume_info="Volume: $(get_volume_info)"
+
+get_brightness_info() {
+    actual_brightness=$(backlight -f /dev/backlight/intel_backlight0 -q)
+    echo $actual_brightness
+}
+brightness_info="Brightness: $(get_brightness_info)"
+
+datentime=$(date "+%Y-%m-%d %H:%M:%S")
+
+echo "$brightness_info | $volume_info | $battery_info | $datentime"
