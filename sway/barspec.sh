64dd0003d5a984e6ca0fbdd66bfa12162153da8c
Jose Esparza <pebeto99@gmail.com>
2025-04-11 11:28:41 -0500

    Configuring it for Linux (bye FreeBSD)

--- sway/barspec.sh
+++ sway/barspec.sh
@@ -1,32 +1,22 @@
 # Battery information
 get_battery_info() {
-    state=$(acpiconf -i $1 | awk '/State/{print $2}')
-    case $state in
-        charging)
-            battery_info="charging"
-            ;;
-        not)
-            battery_info="NA"
-            ;;
-        discharging|high|critical)
-	        battery_info=$(acpiconf -i $1 | awk '/Remaining capacity/{print $3}')
-            ;;
-    esac
-	echo $battery_info
+    percentage=$(cat /sys/class/power_supply/BAT$1/capacity)%
+	echo $percentage
 }
 internal_battery_percentage="IntBAT: $(get_battery_info 0)"
 external_battery_percentage="ExtBAT: $(get_battery_info 1)"
 battery_info="$internal_battery_percentage - $external_battery_percentage"
 
 get_volume_info() {
-    actual_volume=$(mixer vol.volume | awk -F: '{print $2}')
+    actual_volume=$(amixer get Master | grep -o '[0-9]\+%' | head -n 1)
     echo $actual_volume
 }
 volume_info="Volume: $(get_volume_info)"
 
 get_brightness_info() {
-    actual_brightness=$(backlight -f /dev/backlight/intel_backlight0 -q)
-    echo $actual_brightness
+    current_brightness=$(< /sys/class/backlight/intel_backlight/brightness)
+    max_brightness=$(< /sys/class/backlight/intel_backlight/max_brightness)
+    echo $(( current_brightness * 100 / max_brightness ))%
 }
 brightness_info="Brightness: $(get_brightness_info)"
 
